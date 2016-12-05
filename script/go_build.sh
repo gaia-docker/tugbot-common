d829ccfb22c95dda9291deb69d8efa2fc139a658
Effi <effoeffi@gmail.com>
2016-12-05 10:40:05 +0200

    try to fix build;

--- script/go_build.sh
+++ script/go_build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-distdir=.dist
 
 go_build() {
-  rm -rf "${distdir}"
-  mkdir "${distdir}"
   glide install
-  CGO_ENABLED=0 go build -v -o ${distdir}/tugbot-common
+  CGO_ENABLED=0 go build ./...
 }
 
 go_build
