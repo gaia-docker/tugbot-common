ce5455b443f49e45dbd0403982741517b696238c
Effi <effoeffi@gmail.com>
2016-12-07 15:25:28 +0200

    update dependencies;

--- esclient.go
+++ esclient.go
@@ -7,7 +7,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"golang.org/x/net/context"
-	"gopkg.in/olivere/elastic.v5"
+	"gopkg.in/olivere/elastic.v2"
 )
 
 const illegalCharacters = "[:,?<>/\\*?| ]"
